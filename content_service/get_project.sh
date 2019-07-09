e333dc82a3c272713ed73b14fbc53b2f6c7f5f62
twynz <wenyuforwes@163.com>
2019-07-09 18:34:05 +1200

    modify env and little change

--- content_service/get_project.sh
+++ content_service/get_project.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 export CONTENT_DIRECTORY=/src/personal-web-content-service
 echo CONTENT_DIRECTORY
-if [ -d "$CONTENT_DIRECTORY" ]; then
-  cd /src/personal-web-content-service
-  git pull
-else
-  cd /src
-  git clone https://github.com/twynz/personal-web-content-service.git
-fi
+cd /src
+git clone https://github.com/twynz/personal-web-content-service.git
+
--- myauth/get_project.sh
+++ myauth/get_project.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 export AUTH_DIRECTORY=/src/personal-web-auth
 echo $AUTH_DIRECTORY
-if [ -d "$AUTH_DIRECTORY" ]; then
-  cd /src/personal-web-auth
-  git pull
-else
-  cd /src
-  git clone https://github.com/twynz/personal-web-auth.git
-fi
+cd /src
+git clone https://github.com/twynz/personal-web-auth.git
+
