96e040c8805e015491c8a6d555b05bf143319d49
twynz <wenyuforwes@163.com>
2019-07-09 18:34:41 +1200

    add initial script

---
+++ set_env.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+function replace_IP() {
+    sed -i -E 's/=/='$PUBLIC_IP'/g' ./env.sh
+}
+
+function replace_DB() {
+    #clear previous set value
+    sed -i -E 's/USER_DB_PWD=(.*)/USER_DB_PWD=/g' ./env.sh
+    sed -i -E 's/USER_DB_USER=(.*)/USER_DB_USER=/g' ./env.sh
+    sed -i -E 's/USER_DB_ADDRESS=(.*)/USER_DB_ADDRESS=/g' ./env.sh
+    #set db value
+    sed -i -E 's/USER_DB_USER=/USER_DB_USER='$DB_USERNAME'/g' ./env.sh
+    sed -i -E 's/USER_DB_PWD=/USER_DB_PWD='$DB_PASSWORD'/g' ./env.sh
+    sed -i -E 's/USER_DB_ADDRESS=/USER_DB_ADDRESS=personalwebdb.cj6qmmczghu6.ap-southeast-2.rds.amazonaws.com/g' ./env.sh
+}
+
+echo "please input username of db:\n"
+read DB_USERNAME
+echo "please input password of db:\n"
+read DB_PASSWORD
+#get public ip
+PUBLIC_IP="$(wget -qO- https://ipecho.net/plain ; echo)"
+#replace auth
+cd myauth
+replace_IP
+replace_DB
+#replace frontend
+cd ../content_service
+replace_IP
+replace_DB
+#redirect port 80 to 3000
+cd ..
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
+
+
+
+
+
+
